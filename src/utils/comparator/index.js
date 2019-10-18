19e1a3238f30708679fb773dc8cab802cd28d5d8
Mik <mik@opla.ai>
2019-10-18 12:38:45 +0200

    Fix #7

--- src/utils/comparator/index.js
+++ src/utils/comparator/index.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { Comparator } from './TMPComparator';
+import { Comparator } from './Comparator';
 
 import LazzyUnorderedComparator from './LazzyUnorderedComparator';
 import UnorderedComparator from './UnorderedComparator';
