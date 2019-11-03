7ad2deab6b40816554481e5c61f8555614c3ff79
Mik <mik@opla.ai>
2019-11-03 12:33:35 +0100

    Init aice-utils

---
+++ packages/aice-utils/README.md
@@ -0,0 +1,11 @@
+# `aice-utils`
+
+> TODO: description
+
+## Usage
+
+```
+const aiceUtils = require('aice-utils');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/aice-utils/src/index.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+class AIceUtils {
+  constructor() {
+    this.parameters = {};
+  }
+
+  setConfiguration(config) {
+    this.parameters.config = { ...config };
+  }
+
+  getConfiguration() {
+      return this.parameters.config;
+  }
+}
+const singletonInstance = new AIceUtils();
+Object.freeze(singletonInstance);
+export default singletonInstance;
