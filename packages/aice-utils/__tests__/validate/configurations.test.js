ff6dac5b9489aa430086d3aea475b50f46a3e5bb
Mik <mik@opla.ai>
2019-11-03 18:27:38 +0100

    Some validation tests for configuration

--- packages/aice-utils/__tests__/validate/configurations.test.js
+++ packages/aice-utils/__tests__/validate/configurations.test.js
@@ -8,12 +8,40 @@ import { expect } from 'chai';
 import aiceUtils from '../../src';
 
 describe('validate configurations', () => {
-  it('threshold=0.75', async () => {
+  it('valid threshold=0.75', async () => {
     const result = aiceUtils.validateData({ threshold: 0.75 }, 'aice-configuration');
     expect(result).to.eql({ isValid: true });
+  });
+  it('not valid threshold=-0.75', async () => {
+    const result = aiceUtils.validateData({ threshold: -0.75 }, 'aice-configuration');
+    expect(result.isValid).to.equal(false);
+  });
+  it('not valid threshold=-100', async () => {
+    const result = aiceUtils.validateData({ threshold: -0.75 }, 'aice-configuration');
+    expect(result.isValid).to.equal(false);
+  });
+  it('not valid threshold="1"', async () => {
+    const result = aiceUtils.validateData({ threshold: '1' }, 'aice-configuration');
+    expect(result.isValid).to.equal(false);
+  });
+  it('Faulty resolvers', async () => {
+    const result = aiceUtils.validateData({ resolvers: 'error' }, 'aice-configuration');
+    expect(result.isValid).to.equal(false);
   });
   it('Not valid property', async () => {
     const result = aiceUtils.validateData({ dummy: 'value' }, 'aice-configuration');
     expect(result.isValid).to.eql(false);
+  });
+  it("Resolvers can't be empty", async () => {
+    const result = aiceUtils.validateData({ resolvers: [] }, 'aice-configuration');
+    expect(result.isValid).to.equal(false);
+  });
+  it('Resolvers with one simple', async () => {
+    const result = aiceUtils.validateData({ resolvers: [{ name: 'simple' }] }, 'aice-configuration');
+    expect(result.isValid).to.equal(true);
+  });
+  it('Resolvers with one empty object', async () => {
+    const result = aiceUtils.validateData({ resolvers: [{}] }, 'aice-configuration');
+    expect(result.isValid).to.equal(false);
   });
 });
