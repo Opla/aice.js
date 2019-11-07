f6720f670351251c45e13ebc69eeeeefcd7c4a6d
Mik <mik@opla.ai>
2019-11-07 10:03:42 +0100

    feat: change configuration schema

--- packages/aice-utils/__tests__/validate/configurations.test.js
+++ packages/aice-utils/__tests__/validate/configurations.test.js
@@ -9,86 +9,99 @@ import aiceUtils from '../../src';
 
 describe('validate configurations', () => {
   it('valid threshold=0.75', async () => {
-    const result = aiceUtils.validateData({ threshold: 0.75 }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { threshold: 0.75 } }, 'aice-configuration');
     expect(result).to.eql({ isValid: true });
   });
   it('not valid threshold=-0.75', async () => {
-    const result = aiceUtils.validateData({ threshold: -0.75 }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { threshold: -0.75 } }, 'aice-configuration');
     expect(result.isValid).to.equal(false);
   });
   it('not valid threshold=-100', async () => {
-    const result = aiceUtils.validateData({ threshold: -0.75 }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { threshold: -0.75 } }, 'aice-configuration');
     expect(result.isValid).to.equal(false);
   });
   it('not valid threshold="1"', async () => {
-    const result = aiceUtils.validateData({ threshold: '1' }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { threshold: '1' } }, 'aice-configuration');
     expect(result.isValid).to.equal(false);
   });
   it('Faulty resolvers', async () => {
-    const result = aiceUtils.validateData({ resolvers: 'error' }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { resolvers: 'error' } }, 'aice-configuration');
     expect(result.isValid).to.equal(false);
   });
   it('Not valid property', async () => {
-    const result = aiceUtils.validateData({ dummy: 'value' }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { dummy: 'value' } }, 'aice-configuration');
     expect(result.isValid).to.eql(false);
   });
   it("Resolvers can't be empty", async () => {
-    const result = aiceUtils.validateData({ resolvers: [] }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { resolvers: [] } }, 'aice-configuration');
     expect(result.isValid).to.equal(false);
   });
   it('Resolvers with one empty object', async () => {
-    const result = aiceUtils.validateData({ resolvers: [{}] }, 'aice-configuration');
+    const result = aiceUtils.validateData({ configuration: { resolvers: [{}] } }, 'aice-configuration');
     expect(result.isValid).to.equal(false);
   });
   it('Resolvers with one default', async () => {
-    let result = aiceUtils.validateData({ resolvers: [{ name: 'default' }] }, 'aice-configuration');
+    let result = aiceUtils.validateData({ configuration: { resolvers: [{ name: 'default' }] } }, 'aice-configuration');
     expect(result.isValid).to.equal(true);
-    result = aiceUtils.validateData({ resolvers: [{ name: 'default', type: 'resolver' }] }, 'aice-configuration');
+    result = aiceUtils.validateData(
+      { configuration: { resolvers: [{ name: 'default', type: 'resolver' }] } },
+      'aice-configuration',
+    );
     expect(result.isValid).to.equal(true);
-    result = aiceUtils.validateData({ resolvers: [{ name: 'notvalid', type: 'notvalid' }] }, 'aice-configuration');
-    expect(result.isValid).to.equal(false);
-    result = aiceUtils.validateData({ resolvers: [{ name: 'notvalid', novalid: 'notvalid' }] }, 'aice-configuration');
+    result = aiceUtils.validateData(
+      { configuration: { resolvers: [{ name: 'notvalid', type: 'notvalid' }] } },
+      'aice-configuration',
+    );
     expect(result.isValid).to.equal(false);
     result = aiceUtils.validateData(
-      { resolvers: [{ name: 'notvalid', type: 'resolver', novalid: 'notvalid' }] },
+      { configuration: { resolvers: [{ name: 'notvalid', novalid: 'notvalid' }] } },
+      'aice-configuration',
+    );
+    expect(result.isValid).to.equal(false);
+    result = aiceUtils.validateData(
+      { configuration: { resolvers: [{ name: 'notvalid', type: 'resolver', novalid: 'notvalid' }] } },
       'aice-configuration',
     );
     expect(result.isValid).to.equal(false);
   });
   it('Resolvers with one simple', async () => {
     const result = aiceUtils.validateData(
-      { resolvers: [{ name: 'simple', type: 'simple-resolver' }] },
+      { configuration: { resolvers: [{ name: 'simple', type: 'simple-resolver' }] } },
       'aice-configuration',
     );
     expect(result.isValid).to.equal(true);
   });
   it('Resolvers with one remote', async () => {
     let result = aiceUtils.validateData(
-      { resolvers: [{ name: 'simple', type: 'remote-resolver', url: 'http://example.com' }] },
+      { configuration: { resolvers: [{ name: 'simple', type: 'remote-resolver', url: 'http://example.com' }] } },
       'aice-configuration',
     );
     expect(result.isValid).to.equal(true);
     result = aiceUtils.validateData(
-      { resolvers: [{ name: 'simple', type: 'remote-resolver', url: 'notvalid' }] },
+      { configuration: { resolvers: [{ name: 'simple', type: 'remote-resolver', url: 'notvalid' }] } },
       'aice-configuration',
     );
     expect(result.isValid).to.equal(false);
     result = aiceUtils.validateData(
-      { resolvers: [{ name: 'simple', type: 'remote-resolver', url: 'http://example.com', notvalid: 'notvalid' }] },
+      {
+        configuration: {
+          resolvers: [{ name: 'simple', type: 'remote-resolver', url: 'http://example.com', notvalid: 'notvalid' }],
+        },
+      },
       'aice-configuration',
     );
     expect(result.isValid).to.equal(false);
   });
 
   it('valid json string', async () => {
-    const result = aiceUtils.validateData('{ "threshold": 0.75 }', 'aice-configuration');
+    const result = aiceUtils.validateData('{ "configuration": { "threshold": 0.75 } }', 'aice-configuration');
     expect(result).to.eql({ isValid: true });
   });
 
   it('valid json file', async () => {
     const fileManager = {
       getFile: () => ({ type: 'file' }),
-      loadAsJson: () => ({ threshold: 0.75 }),
+      loadAsJson: () => ({ configuration: { threshold: 0.75 } }),
     };
     aiceUtils.setFileManager(fileManager);
     const result = aiceUtils.validateData('filename', 'aice-configuration');
@@ -98,7 +111,7 @@ describe('validate configurations', () => {
   it('non valid json file', async () => {
     const fileManager = {
       getFile: () => ({ notFound: true }),
-      loadAsJson: () => ({ threshold: 0.75 }),
+      loadAsJson: () => ({ configuration: { threshold: 0.75 } }),
     };
     aiceUtils.setFileManager(fileManager);
     const result = aiceUtils.validateData('filename', 'aice-configuration');
